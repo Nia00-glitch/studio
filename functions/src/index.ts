e4519bb0026c0466de19cab513e79d17d1e70b7f
Arsh <arsh94588@gmail.com>
2025-07-14 06:31:07 +0000

    recheck again fix all issues and remove unneccary things which failed fu

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,5 @@
 
 import { onCallGenkit } from '@genkit-ai/firebase';
 import { emergencyFlow } from './simple-flow';
-import { defineSecret } from 'firebase-functions/params';
 
-const GEMINI_API_KEY = defineSecret('GEMINI_API_KEY');
-
-export const simpleGenerate = onCallGenkit(
-  { secrets: [GEMINI_API_KEY] },
-  emergencyFlow
-);
+export const simpleGenerate = onCallGenkit({}, emergencyFlow);
